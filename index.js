11784357989ed76f65e247df4cd5ff8ecb581f85
CharlesAE <charles.ad.edwards@gmail.com>
2023-03-16 13:48:09 -0400

    cors fix

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import standingsRoutes from './routes/standings.js';
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), (req, res,next) => {
+//res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', "*");
+   res.header("Access-Control-Allow-Headers", "accept, content-type");
+   //res.header("Access-Control-Max-Age", "1728000");
+});
 app.get('/', standingsRoutes);
 
 app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
