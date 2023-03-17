06b146b1d86359c1204c01d16d0793692452e743
CharlesAE <charles.ad.edwards@gmail.com>
2023-03-16 23:40:04 -0400

    adjust cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 app.use(bodyParser.json());
 app.use(cors({
-    origin: '*',
+    origin: 'https://the-prem.netlify.app/',
     methods: ['GET']
 }));
 app.get('/', standingsRoutes);
